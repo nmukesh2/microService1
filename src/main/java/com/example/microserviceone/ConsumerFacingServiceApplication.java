8a36de5fb7081742396e7a487eda6ec74588b180
nmukesh2 <dude.nitin18@live.in>
2021-09-26 08:58:52 +0000

    done

--- src/main/java/com/example/microserviceone/ConsumerFacingServiceApplication.java
+++ src/main/java/com/example/microserviceone/ConsumerFacingServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={"com.example.microserviceone.entity"})
 @EntityScan("com.example.microserviceone.entity")
 public class ConsumerFacingServiceApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerFacingServiceApplication.class, args);
 	}
